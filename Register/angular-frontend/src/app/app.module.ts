37b3f02434341303c81af442230adc1f9e5e06fc
basha <d-imtiaz.basha@capgemini.com>
2022-10-04 10:34:40 +0530

    First commit

---
+++ Register/angular-frontend/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { UserComponent } from './user/user.component';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Register/angular-frontend/src/app/user.ts
@@ -0,0 +1,12 @@
+export class User {
+
+    id: number | any;
+    userName: string | any;
+    password: string | any;
+    firstName: string | any;
+    lastName: string | any;
+    gender: string | any;
+    dob: string | any;
+    email: string | any;
+}
+
